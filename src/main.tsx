8cd79b6be1d24dbde725bafd4ba390e3d84c1246
pb-06 <hjozsef91@protonmail.com>
2026-01-16 11:16:47 +0100

    Create Neon Auth logic

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
+import { NeonAuthUIProvider } from '@neondatabase/neon-js/auth/react';
+import '@neondatabase/neon-js/ui/css';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.tsx'
+import { authClient } from './lib/auth';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <NeonAuthUIProvider emailOTP authClient={authClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </NeonAuthUIProvider>
+  </StrictMode>
 )
