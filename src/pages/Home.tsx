90318e242c1183daab201bf75215092256c23cc0
pb-06 <hjozsef91@protonmail.com>
2026-01-16 11:40:26 +0100

    Make Navbar and Tabs menu

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,32 @@
-import {
-    RedirectToSignIn,
-    SignedIn,
-    UserButton,
-} from '@neondatabase/neon-js/auth/react/ui';
+import { RedirectToSignIn, SignedIn, UserButton } from '@neondatabase/neon-js/auth/react/ui';
+import { Mail } from 'lucide-react';
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export function Home() {
     return (
         <>
             <SignedIn>
-                <div className='flex flex-col justify-center items-center min-h-screen gap-8'>
-                    <div style={{ textAlign: 'center' }}>
-                        <h1>Welcome!</h1>
-                        <p>You're successfully authenticated.</p>
+                <div className='container mx-auto px-4 sm:px-6 lg:px-8'>
+                    {/* Navbar */}
+                    <nav className='flex justify-between items-center h-32'>
+                        <div className='flex items-center gap-2'>
+                            <Mail className="text-blue-600" size={28} />
+                            <h1 className="text-2xl font-bold">Üzenetküldő</h1>
+                        </div>
+
                         <UserButton />
-                    </div>
+                    </nav>
+
+                    {/* Tabs */}
+                    <Tabs defaultValue="inbox">
+                        <TabsList className='w-full h-12'>
+                            <TabsTrigger value="inbox">Bejövő üzenetek</TabsTrigger>
+                            <TabsTrigger value="sent">Elküldött üzenetek</TabsTrigger>
+                        </TabsList>
+                        <TabsContent value="inbox">Ide jönnek a bejövő üzenetek</TabsContent>
+                        <TabsContent value="sent">Ide jönnek az elküldött üzenetek</TabsContent>
+                    </Tabs>
                 </div>
             </SignedIn>
             <RedirectToSignIn />
